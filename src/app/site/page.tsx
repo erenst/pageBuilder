70d119db93fc6ae41d7d9994cfaf25caf2f25a74
erenst <erenst2002@hotmail.com>
2025-01-20 20:15:06 +1030

    setup clerk routes

---
+++ src/app/site/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <section className="h-full w-full pt-36 relative flex items-center justify-center flex-col"></section>
+    </main>
+  );
+}
